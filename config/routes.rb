60833a5a00c025f3855c082b29e5ae9a127e9f8a
kuon <can-dle.01-01on@ezweb.ne.jp>
2021-04-21 10:10:44 +0000

    user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "tasks#index"
+  
+  get 'toppages/index'
+  get "signup", to: "users#new"
+  resources :users, only: [:index, :show, :create]
   resources :tasks
 end
