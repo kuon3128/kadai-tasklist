5ccf1f05b9037874c2e93cdac299ec4895c25c2e
kuon <can-dle.01-01on@ezweb.ne.jp>
2021-04-25 13:09:30 +0000

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in
+  
   def index
-    @users = User.order(id: :desc).page(params[:page]).per(10)
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to "login"
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   root to: "tasks#index"
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   
-  get 'toppages/index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   get "signup", to: "users#new"
   resources :users, only: [:index, :show, :create]
   resources :tasks
