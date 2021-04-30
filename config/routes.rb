b219ab497e70f60e4973d37b9355b699dca297a4
kuon <can-dle.01-01on@ezweb.ne.jp>
2021-04-30 07:37:05 +0000

    route rewrite

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
   root to: "tasks#index"
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  get 'toppages/index'
   get "signup", to: "users#new"
   resources :users, only: [:create]
   resources :tasks
