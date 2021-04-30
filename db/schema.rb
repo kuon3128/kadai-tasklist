15d74911f169ba24242a0d826f117d04e43fe4f8
kuon <can-dle.01-01on@ezweb.ne.jp>
2021-04-30 08:24:48 +0000

    migration reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_25_083130) do
+ActiveRecord::Schema.define(version: 2021_04_25_683130) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
